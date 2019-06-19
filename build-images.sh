d9f1cbd2bb447d91c3de8f63ad77db192cc89f7a
rrt8 <rrt8@cdc.gov>
2019-06-19 05:34:41 -0700

    add ci/cd update

--- build-images.sh
+++ build-images.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-docker build -t spark-base:2.4.3 ./docker/base
-docker build -t spark-master:2.4.3 ./docker/spark-master
-docker build -t spark-worker:2.4.3 ./docker/spark-worker
-docker build -t spark-submit:2.4.3 ./docker/spark-submit
+docker build -t eip.ds/spark-base:2.4.3 ./docker/base
+docker build -t eip.ds/spark-master:2.4.3 ./docker/spark-master
+docker build -t eip.ds/spark-worker:2.4.3 ./docker/spark-worker
+docker build -t eip.ds/spark-submit:2.4.3 ./docker/spark-submit
